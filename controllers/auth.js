d52251200a6f2ddb955cbd6b7fd006c5fe59d050
sebastian461 <sebastiano-461@hotmail.com>
2024-01-30 15:58:24 -0500

    Creación de usuarios

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,41 @@
 const { response } = require("express");
+const bcrypt = require("bcryptjs");
+const Usuario = require("../models/Usuario");
 
-const crearUsuario = (req, res = response) => {
-  const { name, email, password } = req.body;
+const crearUsuario = async (req, res = response) => {
+  const { email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: "registro",
-    name,
-    email,
-    password,
-  });
+  try {
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Ese correo ya existe",
+      });
+    }
+
+    usuario = new Usuario(req.body);
+
+    //* Encriptar contraseña
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
+
+    await usuario.save();
+
+    res.status(201).json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 const loginUsuario = (req, res = response) => {
