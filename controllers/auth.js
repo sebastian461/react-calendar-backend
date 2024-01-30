39481cbaf579f52cad899363b41a0a5360ded988
sebastian461 <sebastiano-461@hotmail.com>
2024-01-30 12:10:35 -0500

    Obtención de información del require

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,36 @@
 const { response } = require("express");
 
 const crearUsuario = (req, res = response) => {
+  const { name, email, password } = req.body;
+
+  if (name.length < 1) {
+    return res.status(400).json({
+      ok: false,
+      msg: "Nombre invalido",
+    });
+  }
+
   res.json({
     ok: true,
     msg: "registro",
+    name,
+    email,
+    password,
   });
 };
 
-const loginUsuario = (req, res) => {
+const loginUsuario = (req, res = response) => {
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
     msg: "login",
+    email,
+    password,
   });
 };
 
-const revalidarToken = (req, res) => {
+const revalidarToken = (req, res = response) => {
   res.json({
     ok: true,
     msg: "renew",
--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 //* Directorio publico
 app.use(express.static("public"));
 
+//* Lectura y parseo del body
+app.use(express.json());
+
 //* Rutas
 app.use("/api/auth", require("./routes/auth"));
 
