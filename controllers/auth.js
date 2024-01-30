7f527665556811a91bc541f87523d0c76a8b1ba1
sebastian461 <sebastiano-461@hotmail.com>
2024-01-30 14:20:59 -0500

    Validación de errores (express-validator)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,20 @@
 const { response } = require("express");
+const { validationResult } = require("express-validator");
 
 const crearUsuario = (req, res = response) => {
   const { name, email, password } = req.body;
 
-  if (name.length < 1) {
+  //* Manejo de errores
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
     return res.status(400).json({
       ok: false,
-      msg: "Nombre invalido",
+      errors: errors.mapped(),
     });
   }
 
-  res.json({
+  res.status(201).json({
     ok: true,
     msg: "registro",
     name,
@@ -22,7 +26,16 @@ const crearUsuario = (req, res = response) => {
 const loginUsuario = (req, res = response) => {
   const { email, password } = req.body;
 
-  res.json({
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
+  res.status(202).json({
     ok: true,
     msg: "login",
     email,
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@
 */
 
 const { Router } = require("express");
+const { check } = require("express-validator");
 const {
   crearUsuario,
   loginUsuario,
@@ -11,9 +12,28 @@ const {
 } = require("../controllers/auth");
 const router = Router();
 
-router.post("/new", crearUsuario);
+router.post(
+  "/new",
+  [
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+  ],
+  crearUsuario
+);
 
-router.post("/", loginUsuario);
+router.post(
+  "/",
+  [
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+  ],
+  loginUsuario
+);
 
 router.get("/renew", revalidarToken);
 
