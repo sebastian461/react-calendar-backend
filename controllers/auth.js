7bc47e4c53a6cc57fe8b34f853aadd4d0a9ab5cc
sebastian461 <sebastiano-461@hotmail.com>
2024-01-30 15:28:45 -0500

    Conección con la BD

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,4 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
 
 const crearUsuario = (req, res = response) => {
   const { name, email, password } = req.body;
@@ -15,15 +14,6 @@ const crearUsuario = (req, res = response) => {
 
 const loginUsuario = (req, res = response) => {
   const { email, password } = req.body;
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
 
   res.status(202).json({
     ok: true,
