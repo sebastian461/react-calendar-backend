df402a36cb33711bbee9b7884d4709eda882d628
sebastian461 <sebastiano-461@hotmail.com>
2024-01-31 16:31:58 -0500

    Crear evento

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const Evento = require("../models/Evento");
 
 const getEventos = (req, res = response) => {
   return res.json({
@@ -7,9 +8,24 @@ const getEventos = (req, res = response) => {
   });
 };
 
-const crearEvento = (req, res = response) => {
-  // Verificar que tenga el evento
-  console.log(req.body);
+const crearEvento = async (req, res = response) => {
+  const evento = new Evento(req.body);
+
+  try {
+    evento.user = req.uid;
+    const eventoGuardado = await evento.save();
+
+    res.json({
+      ok: true,
+      evento: eventoGuardado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 
   return res.json({
     ok: true,
